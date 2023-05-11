344126bd364a79a7edbdd4578c04a3d6b0e87ee4
Bill Wake <william.wake@acm.org>
2023-05-11 10:41:37 -0500

    Initial Commit

---
+++ InchCalc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InchCalc
+//
+//  Created by Bill Wake on 5/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InchCalc/InchCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  InchCalcApp.swift
+//  InchCalc
+//
+//  Created by Bill Wake on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InchCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
