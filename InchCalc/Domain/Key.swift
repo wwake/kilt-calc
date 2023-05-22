25d5a25d32126978322315cc604a40625c2fcb5c
Bill Wake <william.wake@acm.org>
2023-05-22 14:39:55 -0500

    Let Key hold an action; manually implemented Equatable and Washable

--- InchCalc/Domain/Key.swift
+++ InchCalc/Domain/Key.swift
@@ -1,10 +1,24 @@
 import Foundation
 
-struct Key: Identifiable, Hashable {
+struct Key: Identifiable {
   let id = UUID()
   let name: String
+  let action: (String) -> Void
 
-  init(_ name: String) {
+  init(_ name: String, _ action: @escaping (String) -> Void = {_ in }) {
     self.name = name
+    self.action = action
+  }
+}
+
+extension Key: Equatable {
+  static func == (lhs: Key, rhs: Key) -> Bool {
+    lhs.id == rhs.id
+  }
+}
+
+extension Key: Hashable {
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
   }
 }
