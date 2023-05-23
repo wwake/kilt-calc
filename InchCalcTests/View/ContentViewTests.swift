72f5f36a60ff619918f4cb6750f542495f06800e
Bill Wake <william.wake@acm.org>
2023-05-23 14:31:30 -0500

    Install EGtest, fix so one parameterized test can run

--- InchCalcTests/View/ContentViewTests.swift
+++ InchCalcTests/View/ContentViewTests.swift
@@ -1,8 +1,20 @@
+import EGTest
 import SwiftUI
 import ViewInspector
 import XCTest
 
 @testable import InchCalc
+
+extension XCTestCase {
+  func check<Input, Output>(
+    _ tests: [EG<Input, Output>],
+    _ assertFunction: (EG<Input, Output>) throws -> Void
+  ) throws {
+    try tests.forEach {
+      try assertFunction($0)
+    }
+  }
+}
 
 final class ContentViewTests: XCTestCase {
   fileprivate func display(_ sut: ContentView) throws -> String {
@@ -10,17 +22,25 @@ final class ContentViewTests: XCTestCase {
   }
 
   fileprivate func key(_ sut: ContentView, _ name: String) throws -> InspectableView<ViewType.Text> {
-    try sut.inspect().find(text: name)
+    try sut.inspect().vStack()[1].find(text: name)
   }
 
-  func testDigitKeyPress() throws {
+  func testAllDigitKeyPresses() throws {
+    try check([
+      EG("0", expect: "0")
+    ]) {
+      try checkDigitKeyPress($0.input, $0.expect)
+    }
+  }
+
+  private func checkDigitKeyPress(_ keyName: String, _ expectedDisplayText: String) throws {
     @StateObject var calculator = Calculator()
 
     let sut = ContentView(calculator: calculator)
-    let key = try key(sut, "8")
+    let key = try key(sut, keyName)
 
     try key.callOnTapGesture()
 
-    XCTAssertEqual(try display(sut), "8")
+    XCTAssertEqual(try display(sut), expectedDisplayText)
   }
 }
