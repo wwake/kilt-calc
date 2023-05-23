abd304d883dd1bd2e536a4f7d15dadb30fe83c50
Bill Wake <william.wake@acm.org>
2023-05-22 22:08:32 -0500

    Fix formatting

--- InchCalc/Domain/Keypad.swift
+++ InchCalc/Domain/Keypad.swift
@@ -5,12 +5,48 @@ public class Keypad: ObservableObject {
 
   public init(_ calculator: Calculator) {
     self.contents = [
-      [Key("C"), Key("("), Key(")"), Key("%"), Key("\u{232B}")],
-      [Key("yd"), Key("ft"), Key("in"), Key("/"), Key("\u{00f7}")],
-      [Key("MC"), Key("7", Calculator.digit(calculator)), Key("8"), Key("9"), Key("\u{00d7}")],
-      [Key("MR"), Key("4"), Key("5"), Key("6"), Key("-")],
-      [Key("M-"), Key("1"), Key("2"), Key("3"), Key("+")],
-      [Key("M+"), Key("0"), Key("\u{22c5}"), Key("\u{00b1}"), Key("=", Calculator.enter(calculator))],
+      [
+        Key("C"),
+        Key("("),
+        Key(")"),
+        Key("%"),
+        Key("\u{232B}")
+      ],
+      [
+        Key("yd"),
+        Key("ft"),
+        Key("in"),
+        Key("/"),
+        Key("\u{00f7}")
+      ],
+      [
+        Key("MC"),
+        Key("7", Calculator.digit(calculator)),
+        Key("8", Calculator.digit(calculator)),
+        Key("9", Calculator.digit(calculator)),
+        Key("\u{00d7}")
+      ],
+      [
+        Key("MR"),
+        Key("4", Calculator.digit(calculator)),
+        Key("5", Calculator.digit(calculator)),
+        Key("6", Calculator.digit(calculator)),
+        Key("-")
+      ],
+      [
+        Key("M-"),
+        Key("1", Calculator.digit(calculator) ),
+        Key("2", Calculator.digit(calculator)),
+        Key("3", Calculator.digit(calculator)),
+        Key("+")
+      ],
+      [
+        Key("M+"),
+        Key("0", Calculator.digit(calculator)),
+        Key("\u{22c5}"),
+        Key("\u{00b1}"),
+        Key("=", Calculator.enter(calculator))
+      ],
     ]
   }
 }
