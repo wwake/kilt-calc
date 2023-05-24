9ed7177dfd08ea46e56cfdee8e4f32d1558cac26
Bill Wake <william.wake@acm.org>
2023-05-24 15:17:16 -0500

    Map numbers to NSNumber when we split them out of pending

--- InchCalc/Domain/Calculator.swift
+++ InchCalc/Domain/Calculator.swift
@@ -49,14 +49,17 @@ public class Calculator: ObservableObject {
     guard !pending.isEmpty else { return }
 
     do {
-      let numbers = try pending.split(separator: Regex("[a-z]+"))
+      let numbers = try pending
+        .split(separator: Regex("[a-z]+"))
+        .map {formatter.number(from: String($0))}
+
       let units = try pending.split(separator: Regex("[0-9]+"))
 
       pending = ""
       if numbers.isEmpty {
         value = Value.error
       } else if numbers.count == 1 && units.count == 0 {
-        let possibleNumber = formatter.number(from: String(numbers[0]))
+        let possibleNumber = numbers[0]
         value = possibleNumber == nil ? .error : .number(possibleNumber!)
       } else if numbers.count != units.count {
         value = .error
@@ -64,7 +67,7 @@ public class Calculator: ObservableObject {
         var inches = 0.0
         zip(numbers, units).forEach { number, unit in
           if unit == "in" {
-            let possibleNumber = formatter.number(from: String(number))!
+            let possibleNumber = number!
             inches += possibleNumber.doubleValue
           }
         }
