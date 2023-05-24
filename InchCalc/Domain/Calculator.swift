821475165b9569746a0f8965f5edcd63e47bc3ea
Bill Wake <william.wake@acm.org>
2023-05-24 13:10:20 -0500

    Introduce a separate pending object to hold entered numbers

--- InchCalc/Domain/Calculator.swift
+++ InchCalc/Domain/Calculator.swift
@@ -1,20 +1,25 @@
 import Foundation
 
 public class Calculator: ObservableObject {
-  @Published public var display: String = "0"
+  public var display: String {
+    pending
+  }
+
   @Published private(set) var alreadyEnteringNewNumber = false
 
+  @Published private(set) var pending: String = "0"
+
   public func clear(_: String) {
-    display = "0"
+    pending = "0"
     alreadyEnteringNewNumber = false
   }
 
   public func digit(_ digit: String) {
     if !alreadyEnteringNewNumber {
-      display = ""
+      pending = ""
       alreadyEnteringNewNumber = true
     }
-    display.append(digit)
+    pending.append(digit)
   }
 
   public func enter(_: String) {
