0b7af1ecc1e31fe7caaf95762ba5f1b907e5a429
Bill Wake <william.wake@acm.org>
2023-05-24 13:34:06 -0500

    Make Value its own enum type

--- InchCalc/Domain/Calculator.swift
+++ InchCalc/Domain/Calculator.swift
@@ -1,16 +1,25 @@
 import Foundation
 
-typealias Value = NSNumber
+public enum Value {
+  case number(NSNumber)
+}
 
 public class Calculator: ObservableObject {
   @Published private(set) var alreadyEnteringNewNumber = false
 
   @Published private(set) var pending: String = ""
 
-  @Published private(set) var value = Value(0)
+  @Published private(set) var value = Value.number(0)
 
   public var display: String {
-    pending.isEmpty ? "\(value)" : pending
+    if !pending.isEmpty { return pending }
+
+    if case let .number(aNumber) = value {
+      let formatter = NumberFormatter()
+      return formatter.string(from: aNumber) ?? ""
+    }
+
+    return "??" // can't happen
   }
 
   public func clear(_: String) {
@@ -28,7 +37,7 @@ public class Calculator: ObservableObject {
 
   public func enter(_: String) {
     let formatter = NumberFormatter()
-    value = formatter.number(from: pending)!
+    value = Value.number(formatter.number(from: pending)!)
     pending = ""
     alreadyEnteringNewNumber = false
   }
