5d720d06713583ba79c67ba7919607fc3f0d169c
Bill Wake <william.wake@acm.org>
2023-06-01 14:39:53 -0500

    Create new Expression class

--- InchCalc/Domain/Calculator.swift
+++ InchCalc/Domain/Calculator.swift
@@ -6,53 +6,8 @@ extension Stack<String> {
   }
 }
 
-public class Calculator: ObservableObject {
-  @Published private(set) var result = Value.number(0)
-
-  @Published private(set) var input = Stack<String>()
-
-  let formatter = NumberFormatter()
-
-  public var display: String {
-    if !input.isEmpty {
-      return input.joined().trimmingCharacters(in: .whitespaces)
-    }
-    return result.format(ImperialFormatter.asYardFeetInches)
-  }
-
-  public func clear(_: String) {
-    result = .number(0)
-    input.clear()
-  }
-
-  public func digit(_ digit: String) {
-    input.push(digit)
-  }
-
-  public func unit(_ value: String) {
-    if !input.isEmpty && input.top.hasSuffix(" ") {
-      _ = input.pop()
-    }
-
-    input.push(" \(value) ")
-  }
-
-  private func isOperator(_ string: String) -> Bool {
-    ["+", "-", Keypad.multiply, Keypad.divide].contains(string)
-  }
-
-  public func op(_ op: String) {
-    if !input.isEmpty {
-      let lastChar = input.top
-      if ["+", "-", Keypad.multiply, Keypad.divide].contains(lastChar) {
-        _ = input.pop()
-      }
-    }
-
-    input.push(op)
-  }
-
-  private func enter2(_ input: Stack<String>) -> Value {
+public class Expression {
+  public func evaluate(_ input: Stack<String>) -> Value {
     if input.isEmpty { return Value.number(0) }
 
     var operands: Stack<Value> = Stack()
@@ -97,12 +52,58 @@ public class Calculator: ObservableObject {
 
     return operands.pop()
   }
+}
+
+public class Calculator: ObservableObject {
+  @Published private(set) var result = Value.number(0)
+  @Published private(set) var input = Stack<String>()
+
+  let formatter = NumberFormatter()
+
+  public var display: String {
+    if !input.isEmpty {
+      return input.joined().trimmingCharacters(in: .whitespaces)
+    }
+    return result.format(ImperialFormatter.asYardFeetInches)
+  }
+
+  public func clear(_: String) {
+    result = .number(0)
+    input.clear()
+  }
+
+  public func digit(_ digit: String) {
+    input.push(digit)
+  }
+
+  public func unit(_ value: String) {
+    if !input.isEmpty && input.top.hasSuffix(" ") {
+      _ = input.pop()
+    }
+
+    input.push(" \(value) ")
+  }
+
+  private func isOperator(_ string: String) -> Bool {
+    ["+", "-", Keypad.multiply, Keypad.divide].contains(string)
+  }
+
+  public func op(_ op: String) {
+    if !input.isEmpty {
+      let lastChar = input.top
+      if ["+", "-", Keypad.multiply, Keypad.divide].contains(lastChar) {
+        _ = input.pop()
+      }
+    }
+
+    input.push(op)
+  }
 
   public func enter(_: String) {
     if !input.isEmpty && isOperator(input.top) {
       result = .error("expression can't end with an operator")
     } else {
-      result = enter2(input)
+      result = Expression().evaluate(input)
     }
     input.clear()
   }
