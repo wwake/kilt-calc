95ac07caf6dc6fc0beb507bb3f0a587ef7342ee5
Bill Wake <william.wake@acm.org>
2023-06-01 15:12:53 -0500

    Create file for InputBuffer

--- InchCalc/Domain/Calculator.swift
+++ InchCalc/Domain/Calculator.swift
@@ -1,39 +1,5 @@
 import Foundation
 
-public struct InputBuffer {
-  var elements: [String]
-
-  public init(_ elements: [String] = []) {
-    self.elements = elements
-  }
-
-  public var isEmpty: Bool {
-    elements.count == 0
-  }
-
-  public var last: String {
-    elements.last!
-  }
-
-  public mutating func add(_ value: String) {
-    elements.append(value)
-  }
-
-  public mutating func removeLastIf(_ condition: (String) -> Bool) {
-    if !isEmpty && condition(last) {
-      elements.removeLast()
-    }
-  }
-
-  public mutating func clear() {
-    elements.removeAll()
-  }
-
-  public func toString() -> String {
-    elements.joined()
-  }
-}
-
 public class Calculator: ObservableObject {
   @Published private(set) var result = Value.number(0)
   @Published private(set) var input = InputBuffer()
---
+++ InchCalc/Utility/InputBuffer.swift
@@ -0,0 +1,33 @@
+public struct InputBuffer {
+  var elements: [String]
+
+  public init(_ elements: [String] = []) {
+    self.elements = elements
+  }
+
+  public var isEmpty: Bool {
+    elements.count == 0
+  }
+
+  public var last: String {
+    elements.last!
+  }
+
+  public mutating func add(_ value: String) {
+    elements.append(value)
+  }
+
+  public mutating func removeLastIf(_ condition: (String) -> Bool) {
+    if !isEmpty && condition(last) {
+      elements.removeLast()
+    }
+  }
+
+  public mutating func clear() {
+    elements.removeAll()
+  }
+
+  public func toString() -> String {
+    elements.joined()
+  }
+}
