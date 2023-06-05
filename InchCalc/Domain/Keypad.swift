73b148198415c34e56d4c98a56c049e66e73bc60
Bill Wake <william.wake@acm.org>
2023-06-05 14:45:39 -0500

    Remove Calculator from Keypad

--- InchCalc/Domain/Keypad.swift
+++ InchCalc/Domain/Keypad.swift
@@ -5,52 +5,48 @@ public struct Keypad {
   static let dot = "\u{22c5}"
   static let plusOrMinus = "\u{00b1}"
 
-  let contents: [[Key]]
-
-  public init(_ calculator: Calculator) {
-    self.contents = [
-      [
-        Key("C", .clear),
-        Key("("),
-        Key(")"),
-        Key("%"),
-        Key("\u{232B}", .backspace),
-      ],
-      [
-        Key("yd", .unit(.yard)),
-        Key("ft", .unit(.foot)),
-        Key("in", .unit(.inch)),
-        Key("/"),
-        Key("\u{00f7}", .divide),
-      ],
-      [
-        Key("MC"),
-        Key("7", .digit(7)),
-        Key("8", .digit(8)),
-        Key("9", .digit(9)),
-        Key("\u{00d7}", .multiply),
-      ],
-      [
-        Key("MR"),
-        Key("4", .digit(4)),
-        Key("5", .digit(5)),
-        Key("6", .digit(6)),
-        Key("-", .subtract),
-      ],
-      [
-        Key("M-"),
-        Key("1", .digit(1)),
-        Key("2", .digit(2)),
-        Key("3", .digit(3)),
-        Key("+", .add),
-      ],
-      [
-        Key("M+"),
-        Key("0", .digit(0)),
-        Key("\u{22c5}"),
-        Key("\u{00b1}"),
-        Key("=", .equals),
-      ],
-    ]
-  }
+  let contents: [[Key]] = [
+    [
+      Key("C", .clear),
+      Key("("),
+      Key(")"),
+      Key("%"),
+      Key("\u{232B}", .backspace),
+    ],
+    [
+      Key("yd", .unit(.yard)),
+      Key("ft", .unit(.foot)),
+      Key("in", .unit(.inch)),
+      Key("/"),
+      Key("\u{00f7}", .divide),
+    ],
+    [
+      Key("MC"),
+      Key("7", .digit(7)),
+      Key("8", .digit(8)),
+      Key("9", .digit(9)),
+      Key("\u{00d7}", .multiply),
+    ],
+    [
+      Key("MR"),
+      Key("4", .digit(4)),
+      Key("5", .digit(5)),
+      Key("6", .digit(6)),
+      Key("-", .subtract),
+    ],
+    [
+      Key("M-"),
+      Key("1", .digit(1)),
+      Key("2", .digit(2)),
+      Key("3", .digit(3)),
+      Key("+", .add),
+    ],
+    [
+      Key("M+"),
+      Key("0", .digit(0)),
+      Key("\u{22c5}"),
+      Key("\u{00b1}"),
+      Key("=", .equals),
+    ],
+  ]
 }
