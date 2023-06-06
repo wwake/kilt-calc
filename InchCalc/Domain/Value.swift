7de30bfbeb0899459dee7e4c066a82db826ea18a
Bill Wake <william.wake@acm.org>
2023-06-06 14:51:27 -0500

    Extract ValueFormatter to its own file

--- InchCalc/Domain/Value.swift
+++ InchCalc/Domain/Value.swift
@@ -119,29 +119,3 @@ extension Value {
     return Value.inches(inches)
   }
 }
-
-public struct ValueFormatter {
-  let formatter: NumberFormatter
-
-  public init() {
-    let formatter = NumberFormatter()
-    formatter.positiveInfinitySymbol = "result too large"
-    formatter.negativeInfinitySymbol = "result too large"
-    formatter.notANumberSymbol = "result can't be determined"
-    self.formatter = formatter
-  }
-
-  public func format(_ imperialFormatter: ImperialFormatterFunction, _ value: Value) -> String {
-    switch value {
-    case .error(let message):
-      return message
-
-    case .number(let aNumber):
-      let result = formatter.string(from: NSNumber(value: aNumber)) ?? "internal error"
-      return "\(result)"
-
-    case .inches(let theInches):
-      return imperialFormatter(formatter, theInches)
-    }
-  }
-}
