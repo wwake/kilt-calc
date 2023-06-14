c45d4768c322cc3765c2256823dd1a542cb37a5f
Bill Wake <william.wake@acm.org>
2023-06-14 15:56:38 -0500

    Move methods into SplitDouble

--- KiltCalc/Domain/ValueFormatter.swift
+++ KiltCalc/Domain/ValueFormatter.swift
@@ -5,8 +5,34 @@ public struct SplitDouble {
   public var wholeNumber: Int
   public var fraction: Double
 
+  public init(_ aNumber: Double) {
+    let (wholePart, fractionPart) = modf(aNumber)
+
+    signum = aNumber < 0 ? -1 : 1
+    wholeNumber = abs(Int(wholePart))
+    fraction = abs(fractionPart)
+  }
+
+  public init(signum: Int, wholeNumber: Int, numerator: Int, denominator: Int) {
+    self.signum = signum
+    self.wholeNumber = abs(wholeNumber)
+    self.fraction = abs(Double(numerator) / Double(denominator))
+  }
+
   public var asDouble: Double {
     Double(signum) * (Double(wholeNumber) + fraction)
+  }
+
+  fileprivate func fractionParts(_ roundingDenominator: Int) -> (Int, Int) {
+    var denominator = roundingDenominator
+    var numerator = Int(round(fraction * Double(denominator)))
+
+    if numerator.isMultiple(of: 2) {
+      numerator /= 2
+      denominator /= 2
+    }
+
+    return (numerator, denominator)
   }
 }
 
@@ -22,13 +48,6 @@ public struct ValueFormatter {
     self.formatter = formatter
   }
 
-  fileprivate func split(_ aNumber: Double) -> SplitDouble {
-    let (whole, fraction) = modf(aNumber)
-    let sign = aNumber < 0 ? -1 : 1
-    let wholeNumber = Int(whole)
-    return SplitDouble(signum: sign, wholeNumber: abs(wholeNumber), fraction: abs(fraction))
-  }
-
   fileprivate func findAdjustment(_ aNumber: Double, _ split: SplitDouble) -> String {
     let computed = split.asDouble
     if computed < aNumber {
@@ -40,45 +59,48 @@ public struct ValueFormatter {
     return ""
   }
 
+  fileprivate func formatWholeAndFraction(_ wholeNumber: Int, _ numerator: Int, _ denominator: Int) -> String {
+    let hasFraction = numerator != 0 && numerator != denominator
+
+    var result = ""
+    if wholeNumber != 0 || !hasFraction {
+      result += "\(wholeNumber)"
+    }
+
+    if hasFraction {
+      if wholeNumber != 0 {
+        result += "·"
+      }
+      result += "\(numerator)/\(denominator)"
+    }
+    return result
+  }
+
   fileprivate func formatWithFraction(_ aNumber: (Double)) -> String {
     if aNumber.isNaN || aNumber.isInfinite {
       return formatter.string(from: NSNumber(value: aNumber)) ?? "internal error"
     }
 
-    let splitNumber = split(aNumber)
+    let splitNumber = SplitDouble(aNumber)
     let signum = splitNumber.signum
     var wholeNumber = splitNumber.wholeNumber
-    let fraction = splitNumber.fraction
 
-    var denominator = roundingDenominator
-    var numerator = Int(round(fraction * Double(denominator)))
-
-    if numerator.isMultiple(of: 2) {
-      numerator /= 2
-      denominator /= 2
-    }
+    let (numerator, denominator) = splitNumber.fractionParts(roundingDenominator)
 
     if numerator == denominator {
       wholeNumber += 1
     }
 
+    var signMarker = signum < 0 ? "-" : ""
+
+    var formattedNumber = formatWholeAndFraction(wholeNumber, numerator, denominator)
+
     let adjustment = findAdjustment(
       aNumber,
-      SplitDouble(signum: signum, wholeNumber: wholeNumber, fraction: Double(numerator) / Double(denominator))
+      SplitDouble(signum: signum, wholeNumber: wholeNumber, numerator: numerator, denominator: denominator)
     )
 
-    let signMarker = signum < 0 ? "-" : ""
-
-    var result = signMarker
-    if numerator == 0 || numerator == denominator {
-      result += "\(wholeNumber)"
-    } else
-    if wholeNumber == 0 {
-      result += "\(numerator)/\(denominator)"
-    } else {
-      result += "\(wholeNumber)·\(numerator)/\(denominator)"
-    }
-    return result + adjustment
+    return signMarker + formattedNumber + adjustment
   }
 
   public func format(_ imperialFormatter: ImperialFormatterFunction, _ value: Value) -> String {
