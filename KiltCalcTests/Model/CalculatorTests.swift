3d0987605ab13d2d938387e4d2ca5ba05e3688c1
Bill Wake <william.wake@acm.org>
2023-06-15 16:32:52 -0500

    Move remaining ContentViewTests to CalculatorTests

--- KiltCalcTests/Model/CalculatorTests.swift
+++ KiltCalcTests/Model/CalculatorTests.swift
@@ -124,6 +124,49 @@ final class CalculatorTests: XCTestCase {
     XCTAssertEqual(display("4C"), "0")
   }
 
+  func test_UnitsAndOperations() {
+    check([
+      EG("1=", expect: "1"),
+      EG("1yd 5ft 11in =", expect: "2 yd 2 ft 11 in", "all units"),
+      EG("1+5=", expect: "6", "Number addition"),
+      EG("5-1=", expect: "4", "Number subtraction"),
+      EG("5-3-2=", expect: "0", "Associativity of -"),
+      EG("5*3*2=", expect: "30", "Associativity of *"),
+      EG("5+3*2=", expect: "11", "Precedence of * over +"),
+      EG("6:3=", expect: "2", "Number division"),
+      EG("5ft", expect: "5 ft", "Typing unit inserts spaces"),
+      EG("2yd5ft", expect: "2 yd 5 ft", "Typing unit inserts spaces"),
+      EG("ft 3 in 2=", expect: "1 yd 2 in", "Units may precede numbers"),
+      EG("2 yd ft", expect: "2 ft", "Last unit takes precedence"),
+      EG("2 yd ft =", expect: "2 ft", "Last unit takes precedence"),
+      EG("5 ft 2 =", expect: "numbers and units don't match", "more numbers than units"),
+
+      EG("5 ft 2 in <<12 in=", expect: "2 yd", "Backspace"),
+      EG("<<12 in=", expect: "1 ft", "Backspace at start"),
+
+      EG("12~=", expect: "-12"),
+
+      EG("0=", expect: "0"),
+      EG("0~=", expect: "0"),
+      EG("0in~=", expect: "0 in"),
+
+      EG("(2+1)*3=", expect: "9"),
+      EG("(2+1)*(3-1)=", expect: "6"),
+      EG("(13)=", expect: "13"),
+      EG("(2*(((3)*4)+2))=", expect: "28"),
+      EG("2*(((12):4)+2)=", expect: "10"),
+      EG("(2*(((12):4)+2))=", expect: "10"),
+      EG("(11 in-1 in)*3=", expect: "2 ft 6 in"),
+      EG("7in + (11 in+1 in)*3=", expect: "1 yd 7 in"),
+
+      EG("1+(3+4))=", expect: "error - unbalanced parentheses"),
+      EG(")1+2=", expect: "error - unbalanced parentheses"),
+      EG(")1+2(=", expect: "error - unbalanced parentheses"),
+    ]) {
+      EGAssertEqual(display("Y" + $0.input), $0)
+    }
+  }
+
   func test_Evaluation() {
     check([
       EG("4=2", expect: "2", "display shows new expression"),
