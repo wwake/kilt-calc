8ade3a4ce579fd6221ee4f1a2e451427fd48e74b
Bill Wake <william.wake@acm.org>
2023-06-15 12:43:08 -0500

    Add background

--- KiltCalc/View/ContentView.swift
+++ KiltCalc/View/ContentView.swift
@@ -14,43 +14,50 @@ struct ContentView: View {
   let roundingDenominators = [8, 16]
 
   var body: some View {
-    VStack {
-      Text(calculator.display)
-        .accessibilityLabel("display")
-        .padding(4)
-        .frame(width: 330, alignment: .trailing)
-        .border(Color.black)
+    ZStack {
+      Image(decorative: "Background")
+        .resizable()
+        .ignoresSafeArea()
 
-      HStack {
-        Picker("Unit Format", selection: $selectedUnitFormat) {
-          ForEach(ImperialFormatter.allCases) {
-            Text($0.rawValue)
+      VStack {
+        Text(calculator.display)
+          .accessibilityLabel("display")
+          .padding(4)
+          .frame(width: 330, alignment: .trailing)
+          .background(Color.white)
+          .border(Color.black)
+
+        HStack {
+          Picker("Unit Format", selection: $selectedUnitFormat) {
+            ForEach(ImperialFormatter.allCases) {
+              Text($0.rawValue)
+            }
           }
+          .onChange(of: selectedUnitFormat) {
+            calculator.imperialFormat = $0
+          }
+          .accessibilityLabel("unitFormat")
+          .pickerStyle(.menu)
         }
-        .onChange(of: selectedUnitFormat) {
-          calculator.imperialFormat = $0
-        }
-        .accessibilityLabel("unitFormat")
-        .pickerStyle(.menu)
-      }
 
-      LazyVGrid(columns: columns) {
-        ForEach(keypad.contents, id: \.self) { row in
-          Group { // GridRow {
-            ForEach(row) { key in
-              Button(key.name) {
-               calculator.enter(key.entry)
+        LazyVGrid(columns: columns) {
+          ForEach(keypad.contents, id: \.self) { row in
+            Group { // GridRow {
+              ForEach(row) { key in
+                Button(key.name) {
+                  calculator.enter(key.entry)
+                }
+                .frame(width: 60, height: 60)
+                .background(Color("KeyColor"))
               }
-              .frame(width: 60, height: 60)
-              .background(Color("KeyColor"))
             }
           }
         }
       }
+      .onAppear { self.didAppear?(self) } // for ViewInspector
+      .font(.largeTitle)
+      .padding()
     }
-    .onAppear { self.didAppear?(self) } // for ViewInspector
-    .font(.largeTitle)
-    .padding()
   }
 }
 
