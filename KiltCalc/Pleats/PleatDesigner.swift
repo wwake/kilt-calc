49d028ab96ece8288bbbca93a7ea746815621c05
Bill Wake <william.wake@acm.org>
2023-06-29 13:15:47 -0500

    Create initial version of PleatDesigner

---
+++ KiltCalc/Pleats/PleatDesigner.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public class PleatDesigner: ObservableObject {
+  @Published public var notes = ""
+}
---
+++ KiltCalcTests/Pleats/PleatDesignerTests.swift
@@ -0,0 +1,10 @@
+@testable import KiltCalc
+import XCTest
+
+@MainActor
+final class PleatDesignerTests: XCTestCase {
+  func test_initializesCorrectly() throws {
+    let designer = PleatDesigner()
+    XCTAssertEqual(designer.notes, "")
+  }
+}
