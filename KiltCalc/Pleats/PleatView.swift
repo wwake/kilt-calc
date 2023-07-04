7c370aee035259956d1ec9d51f3a13bfb0753f75
Bill Wake <william.wake@acm.org>
2023-07-04 14:23:27 -0500

    Put form & picture in a scrollview; forced the form's height to 550 for now. (Tries with GeometryReader doesn't work as expected.)

--- KiltCalc/Pleats/PleatView.swift
+++ KiltCalc/Pleats/PleatView.swift
@@ -23,42 +23,50 @@ struct PleatView: View {
 
   var body: some View {
     NavigationView {
-      VStack {
-        Form {
-          LabeledContent {
-            TextField("Notes", text: $designer.notes, prompt: Text("Name, tartan, or other notes"))
-              .lineLimit(3)
-          } label: {
-            Text("Notes")
-              .bold()
+      ScrollView(.vertical) {
+        VStack {
+          Form {
+            LabeledContent {
+              TextField("Notes", text: $designer.notes, prompt: Text("Name, tartan, or other notes"))
+                .lineLimit(3)
+            } label: {
+              Text("Notes")
+                .bold()
+            }
+
+            Section("Required") {
+              field("Hip to Hip (rear)", $designer.hipToHipMeasure)
+              field("Sett", $designer.sett)
+              field("Setts/Pleat", $designer.settsPerPleat)
+            }
+
+            Section("Adjustable") {
+              field("#Pleats", $designer.pleatCount)
+                .foregroundColor(designer.needsRequiredValues ? Color.gray : Color.black)
+                .disabled(designer.needsRequiredValues)
+
+              field("Pleat Width", $designer.pleatWidth)
+                .foregroundColor(designer.needsRequiredValues ? Color.gray : Color.black)
+                .disabled(designer.needsRequiredValues)
+            }
+
+            Section {
+              LabeledContent {
+                Text(formatOptional(designer.gap))
+              } label: {
+                Text("Gap")
+              }
+              .foregroundColor(designer.needsRequiredValues ? Color.gray : Color.black)
+              .disabled(designer.needsRequiredValues)
+            }
           }
+          .frame(height: 550)
 
-          field("Hip to Hip (rear)", $designer.hipToHipMeasure)
-          field("Sett", $designer.sett)
-          field("Setts/Pleat", $designer.settsPerPleat)
+          PleatDrawing()
 
-          field("#Pleats", $designer.pleatCount)
-            .foregroundColor(designer.needsRequiredValues ? Color.gray : Color.black)
-            .disabled(designer.needsRequiredValues)
-
-          field("Pleat Width", $designer.pleatWidth)
-            .foregroundColor(designer.needsRequiredValues ? Color.gray : Color.black)
-            .disabled(designer.needsRequiredValues)
-
-          LabeledContent {
-            Text(formatOptional(designer.gap))
-          } label: {
-            Text("Gap")
-          //    .bold()
-          }
-          .foregroundColor(designer.needsRequiredValues ? Color.gray : Color.black)
-          .disabled(designer.needsRequiredValues)
+          Spacer()
+          Spacer()
         }
-
-        PleatDrawing()
-
-        Spacer()
-        Spacer()
       }
       .navigationTitle("Pleats")
     }
