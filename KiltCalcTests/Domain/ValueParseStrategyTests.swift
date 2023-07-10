ec89b026a8b8cfafb56bdb7a5943e969098c994c
Bill Wake <william.wake@acm.org>
2023-07-10 14:21:07 -0500

    Add tests for inches input

--- KiltCalcTests/Domain/ValueParseStrategyTests.swift
+++ KiltCalcTests/Domain/ValueParseStrategyTests.swift
@@ -32,4 +32,29 @@ final class ValueParseStrategyTests: XCTestCase {
       EGAssertEqual(try ValueParseStrategy().parse($0.input), $0)
     }
   }
+
+  func test_InchesWithOrWithoutDecimalPointsOrFractions() throws {
+    try check([
+      EG("/in", expect: .error("can't start with '/'")),
+      EG("/31 in", expect: .error("can't start with '/'")),
+      EG("3/in", expect: .error("missing denominator")),
+      EG("314.1/in", expect: .error("missing denominator")),
+
+      EG("1// in", expect: .error("simple fractions only (at most one '/'")),
+      EG("1///in", expect: .error("simple fractions only (at most one '/'")),
+      EG("314.1//in", expect: .error("simple fractions only (at most one '/'")),
+      EG("3//4in", expect: .error("simple fractions only (at most one '/'")),
+      EG("1/2/3in", expect: .error("simple fractions only (at most one '/'")),
+
+      EG("3/4 in", expect: .inches(0.75), "numerator and denominator"),
+
+      EG("1.25in", expect: .inches(1.25), "simple decimal"),
+      EG("1..25in", expect: .error("too many '.'")),
+      EG("1.2.3in", expect: .error("too many '.'")),
+
+      EG("314.1/2in", expect: .inches(314.5), "explicit fraction"),
+    ]) {
+      EGAssertEqual(try ValueParseStrategy().parse($0.input), $0)
+    }
+  }
 }
