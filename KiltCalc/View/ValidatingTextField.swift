ec319519379fda18f7a040498bfa03542f24cb92
Bill Wake <william.wake@acm.org>
2023-07-17 14:06:39 -0500

    Extract method

--- KiltCalc/View/ValidatingTextField.swift
+++ KiltCalc/View/ValidatingTextField.swift
@@ -2,26 +2,30 @@ import SwiftUI
 
 struct ValidatingTextField: View {
   var label: String
-  @State var input: String = ""
-  @State var errorMessage: String = ""
   @Binding var bound: Value?
 
-  func field2() -> some View {
+  @State var input: String = ""
+  @State var errorMessage: String = ""
+
+  func updateBoundValue(_ input: String) {
+    do {
+      let value = try Value.parse(input)
+      bound = value
+      errorMessage = ""
+    } catch {
+      bound = nil
+      let knownError = error as? String
+      errorMessage = knownError != nil ? knownError!.description : "\(error)"
+    }
+  }
+
+  var body: some View {
     VStack {
       LabeledContent {
         TextField(label, text: $input)
           .multilineTextAlignment(.trailing)
           .keyboardType(.decimalPad)
-          .onChange(of: input) { input in
-            do {
-              let value = try Value.parse(input)
-              bound = value
-              errorMessage = ""
-            } catch {
-              let knownError = error as? String
-              errorMessage = knownError != nil ? knownError!.description : "\(error)"
-            }
-          }
+          .onChange(of: input, perform: updateBoundValue)
       } label: {
         Text(label)
           .bold()
@@ -35,9 +39,5 @@ struct ValidatingTextField: View {
           .foregroundColor(Color.red)
       }
     }
-  }
-
-  var body: some View {
-    field2()
   }
 }
