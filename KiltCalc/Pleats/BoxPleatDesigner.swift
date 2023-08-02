01721d164c8c1f9a91da108e8652f2324a3fbbc5
Bill Wake <william.wake@acm.org>
2023-08-02 15:16:22 -0500

    Extract PleatDesigner protocol to its own file

--- KiltCalc/Pleats/BoxPleatDesigner.swift
+++ KiltCalc/Pleats/BoxPleatDesigner.swift
@@ -1,25 +1,6 @@
 import Foundation
 import SwiftUI
 
-public protocol PleatDesigner: ObservableObject {
-  var needsRequiredValues: Bool { get }
-
-  var idealHip: Value? { get set }
-  var adjustedHip: Value? { get }
-  var hipWasAdjusted: Bool { get }
-
-  var pleatFabric: Double? { get set }
-  var pleatCount: Int { get set }
-  var pleatWidth: Value? { get set }
-
-  var gap: Double? { get }
-  var absoluteGap: Double? { get }
-  var gapRatio: Double { get }
-  var gapLabel: String { get }
-
-  var totalFabric: Double? { get }
-}
-
 public class BoxPleatDesigner: ObservableObject, PleatDesigner {
   private var equations = BoxPleatEquations()
 
