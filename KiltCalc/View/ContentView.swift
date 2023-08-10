ba8bbba0b807997cf2831118019ad67743266723
Bill Wake <william.wake@acm.org>
2023-08-10 13:15:42 -0500

    Make it so we save which tab we're on

--- KiltCalc/View/ContentView.swift
+++ KiltCalc/View/ContentView.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
+enum TopLevelTab: String {
+  case calculator
+  case apronPleat
+  case pleats
+}
+
 struct ContentView: View {
   @ObservedObject var calculator: Calculator
   @StateObject var tartan = TartanDesign()
 
+  @AppStorage("topLevelTab") var topLevelTab: TopLevelTab = .calculator
+
   var body: some View {
-    TabView {
+    TabView(selection: $topLevelTab) {
       ZStack {
         Image(decorative: "Background")
           .resizable()
@@ -15,6 +23,7 @@ struct ContentView: View {
       .tabItem {
         Label("Calculator", systemImage: "grid")
       }
+      .tag(TopLevelTab.calculator)
 
       ZStack {
         Image(decorative: "Background")
@@ -26,11 +35,13 @@ struct ContentView: View {
       .tabItem {
         Label("Apron/Pleat", systemImage: "circle.bottomhalf.filled")
       }
+      .tag(TopLevelTab.apronPleat)
 
       PleatView(tartan: tartan)
       .tabItem {
         Label("Pleats", systemImage: "rectangle.split.3x1")
       }
+      .tag(TopLevelTab.pleats)
     }
     .preferredColorScheme(.light)
   }
