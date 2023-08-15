3c7c4f7f64d3730b29cacedb8f3add2d2d275a99
Bill Wake <william.wake@acm.org>
2023-08-15 15:21:53 -0500

    Improve tests

--- KiltCalcTests/ApronPleats/KiltMeasuresTests.swift
+++ KiltCalcTests/ApronPleats/KiltMeasuresTests.swift
@@ -1,3 +1,4 @@
+import EGTest
 @testable import KiltCalc
 import XCTest
 
@@ -12,27 +13,16 @@ final class KiltMeasuresTests: XCTestCase {
     XCTAssertEqual(m.idealLength, 25)
   }
 
-  func test_HipValueCopiedWhenHipBiggerThanWaist() throws {
-    let m = KiltMeasures()
-    m.actualWaist = 30
-    m.actualHips = 31
-
-    XCTAssertEqual(m.idealHips, 31)
-  }
-
-  func test_HipValueCopiedWhenHipEqualWaist() throws {
-    let m = KiltMeasures()
-    m.actualWaist = 30
-    m.actualHips = 30
-
-    XCTAssertEqual(m.idealHips, 30)
-  }
-
-  func test_UseWaistForHipsWhenHipsSmallerThanWaist() throws {
-    let m = KiltMeasures()
-    m.actualWaist = 30
-    m.actualHips = 29
-
-    XCTAssertEqual(m.idealHips, 30)
+  func test_IdealHips() {
+    check(
+      eg((30.0, 31.0), expect: 31.0, "hip > waist"),
+      eg((30.0, 30.0), expect: 30.0, "hip = waist"),
+      eg((30.0, 29.0), expect: 30.0, "hip < waist")
+    ) {
+      let m = KiltMeasures()
+      m.actualWaist = $0.input.0
+      m.actualHips = $0.input.1
+      EGAssertEqual(m.idealHips, $0)
+    }
   }
 }
