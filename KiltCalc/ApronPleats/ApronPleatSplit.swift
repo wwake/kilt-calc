b4d9546cafb9e6ace5562450c84e4d5c234fa8d2
Bill Wake <william.wake@acm.org>
2023-08-16 14:23:30 -0500

    Move ApronPleatSplit to its own file

---
+++ KiltCalc/ApronPleats/ApronPleatSplit.swift
@@ -0,0 +1,20 @@
+struct ApronPleatSplit {
+  let equalSplit: Double
+  var pleatSteals: Double = 0.0
+
+  var apron: Double {
+    equalSplit - pleatSteals
+  }
+
+  var pleat: Double {
+    equalSplit + pleatSteals
+  }
+
+  init(_ total: Double) {
+    self.equalSplit = total / 2.0
+  }
+
+  mutating func givePleat(_ amount: Double) {
+    pleatSteals = amount
+  }
+}
--- KiltCalcTests/ApronPleats/ApronPleatSplitTests.swift
+++ KiltCalcTests/ApronPleats/ApronPleatSplitTests.swift
@@ -1,27 +1,6 @@
 @testable import KiltCalc
 import XCTest
 
-struct ApronPleatSplit {
-  let equalSplit: Double
-  var pleatSteals: Double = 0.0
-
-  var apron: Double {
-    equalSplit - pleatSteals
-  }
-
-  var pleat: Double {
-    equalSplit + pleatSteals
-  }
-
-  init(_ total: Double) {
-    self.equalSplit = total / 2.0
-  }
-
-  mutating func givePleat(_ amount: Double) {
-    pleatSteals = amount
-  }
-}
-
 @MainActor
 final class ApronPleatSplitTests: XCTestCase {
   func test_SplitStartsInHalf() throws {
