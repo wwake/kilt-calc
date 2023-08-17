0211bf87554d3319441f5aa79fe30e4aa1f87f1d
Bill Wake <william.wake@acm.org>
2023-08-17 14:12:18 -0500

    Check that changing scenarios clears, and adds new scenario if it should

--- KiltCalcTests/ApronPleats/ScenariosTests.swift
+++ KiltCalcTests/ApronPleats/ScenariosTests.swift
@@ -26,4 +26,19 @@ final class ScenariosTests: XCTestCase {
     scenarios.append(KiltMeasures(actualWaist: nil, actualHips: 42))
     XCTAssertEqual(scenarios.scenarios.count, 0)
   }
+
+  func test_changeScenariosClears_EvenIfNewScenariosNotAllowed() {
+    let scenarios = Scenarios()
+    scenarios.append(KiltMeasures(actualWaist: 40, actualHips: 42))
+    scenarios.changeScenarios(false, KiltMeasures(actualWaist: nil, actualHips: 42))
+    XCTAssertEqual(scenarios.scenarios.count, 0)
+  }
+
+  func test_changeScenariosAddsNewScenario_IfNewScenarioIsAllowed() {
+    let scenarios = Scenarios()
+    scenarios.append(KiltMeasures(actualWaist: 30, actualHips: 42))
+    scenarios.changeScenarios(true, KiltMeasures(actualWaist: 44, actualHips: 42))
+    XCTAssertEqual(scenarios.scenarios.count, 1)
+    XCTAssertEqual(scenarios.scenarios.first![.waist]!.apron, 22.0)
+  }
 }
