7c5cfdd92c00654a49d19a094e73474b3bd30c7c
Bill Wake <william.wake@acm.org>
2024-09-10 14:33:19 -0400

    Test removeLastIf and removeLastWhile

---
+++ KiltCalcTests/Utility/InputBufferTests.swift
@@ -0,0 +1,34 @@
+@testable import KiltCalc
+import XCTest
+
+final class InputBufferTests: XCTestCase {
+  func test_removeLastIf_failed_condition() {
+    var sut = InputBuffer([.unit(.inch)])
+    sut.removeLastIf { $0.isBinaryOperator() }
+    XCTAssertEqual(sut.count, 1)
+  }
+
+  func test_removeLastIf_successful_condition() {
+    var sut = InputBuffer([.unit(.inch)])
+    sut.removeLastIf { $0.isUnit() }
+    XCTAssertEqual(sut.count, 0)
+  }
+
+  func test_removeLastWhile_failed_condition() {
+    var sut = InputBuffer([.unit(.inch)])
+    sut.removeLastWhile { $0.isBinaryOperator() }
+    XCTAssertEqual(sut.count, 1)
+  }
+
+  func test_removeLastWhile_succeeds_and_empties_list() {
+    var sut = InputBuffer([.unit(.inch), .unit(.inch)])
+    sut.removeLastWhile { $0.isUnit() }
+    XCTAssertEqual(sut.count, 0)
+  }
+
+  func test_removeLastWhile_successful_condition_leaves_nonempty_list() {
+    var sut = InputBuffer([.dot, .unit(.inch), .unit(.inch)])
+    sut.removeLastWhile { $0.isUnit() }
+    XCTAssertEqual(sut.count, 1)
+  }
+}
