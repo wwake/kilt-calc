ee9eb7bbc00985a48cce79cd5164217432e778e2
Bill Wake <william.wake@acm.org>
2024-09-11 15:30:10 -0400

    Remove start of NewFractionKeyboard; UI tweaks on button

--- KiltCalc/View/FractionKeyboard.swift
+++ KiltCalc/View/FractionKeyboard.swift
@@ -1,39 +1,12 @@
 import SwiftUI
 
-private struct NewFractionKeyboard: ViewModifier {
-  @Binding var slashIsPressed: Bool
-
-  func body(content: Content) -> some View {
-    content
-      .toolbar {
-        ToolbarItem(placement: .keyboard) {
-          HStack {
-            Button("Done") {
-              //focusedField.wrappedValue = nil
-            }
-            Spacer()
-            Button(action: { slashIsPressed = true }) {
-              HStack {
-                Spacer()
-                Text("/")
-                  .bold()
-                Spacer()
-              }
-              .padding(2)
-              .background(Color.green)
-              .frame(width: 100)
-            }
-          }
-        }
-      }
-  }
-}
-
 private struct FractionKeyboard<FOCUS: Hashable>: ViewModifier {
   @Binding var slashIsPressed: Bool
   var focusedField: FocusState<FOCUS?>.Binding
 
   // see https://stackoverflow.com/questions/56491386/how-to-hide-keyboard-when-using-swiftui
+  // and https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-a-toolbar-to-the-keyboard
+
   func body(content: Content) -> some View {
     content
       .toolbar {
@@ -53,13 +26,13 @@ private struct FractionKeyboard<FOCUS: Hashable>: ViewModifier {
               .padding(4)
               .background(
                   RoundedRectangle(
-                      cornerRadius: 10,
+                      cornerRadius: 5,
                       style: .continuous
                   )
                   .fill(.white)
                   .stroke(.gray, lineWidth: 1)
               )
-              .frame(width: 100)
+              .frame(width: 105)
             }
           }
         }
